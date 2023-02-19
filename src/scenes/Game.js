5f1f1880a9dff4f7be65512f6f47dd7a7ca4b47c
Victor Avalos Torrez <tigervat10@gmail.com>
2023-02-19 19:37:04 -0400

    Platform world insert

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,10 +5,17 @@ class Game extends Phaser.Scene {
   }
   preload() {
     this.load.image("background", "./img/background.png");
-    // this.load.image("gameover", "../resources/congratulations.png");
+    this.load.image("gameover", "./img/congratulations.png");
+    this.load.image("platform", "./img/platform.png");
   }
   create() {
     this.add.image(400, 250, "background");
+    this.gameoverImage = this.add.image(400, 90, "gameover");
+    this.gameoverImage.visible = false;
+
+    this.platform = this.physics.add.image(400, 460, "platform");
+    this.platform.body.allowGravity = false;
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 }
 export default Game;
